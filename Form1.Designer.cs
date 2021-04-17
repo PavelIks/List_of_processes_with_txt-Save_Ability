e3128d9b5a67a347e6aa005b768f7a5f8755ee18
Pavlo Ocheretianyi <pavel_ocheretyany_01@mail.ua>
2021-04-17 17:10:17 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 400);
+            this.Text = "Form1";
+
+            killProcessBtn = new Button();
+            killProcessBtn.Location = new System.Drawing.Point(10, 350);
+            killProcessBtn.Size = new System.Drawing.Size(100, 30);
+            killProcessBtn.Text = "save";
+            killProcessBtn.Click += killProcessClick;
+
+            exitBtn = new Button();
+            exitBtn.Location = new System.Drawing.Point(120, 350);
+            exitBtn.Size = new System.Drawing.Size(100, 30);
+            exitBtn.Text = "exit";
+            exitBtn.Click += exitClick;
+
+            startBtn = new Button();
+            startBtn.Location = new System.Drawing.Point(230, 350);
+            startBtn.Size = new System.Drawing.Size(100, 30);
+            startBtn.Text = "start";
+            startBtn.Click += startClick;
+
+            processLV = new ListView();
+            processLV.Location = new System.Drawing.Point(0, 0);
+            processLV.Size = new System.Drawing.Size(800, 330);
+            processLV.View = View.List;
+
+            this.Controls.Add(killProcessBtn);
+            this.Controls.Add(exitBtn);
+            this.Controls.Add(startBtn);
+            this.Controls.Add(processLV);
+        }
+
+
+        Button killProcessBtn;
+        Button exitBtn;
+        Button startBtn;
+        ListView processLV;
+        #endregion
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void startClick(object sender, EventArgs e)
+        {
+            foreach (var item in Process.GetProcesses())
+            {
+                ListViewItem lvi = new ListViewItem();
+                lvi.Text = $"ID: { item.Id} Name: {item.ProcessName} Thread num: {item.Threads} Handle num: {item.HandleCount}";
+                processLV.Items.Add(lvi);
+            }
+        }
+
+        private void killProcessClick(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            if (saveFileDialog.ShowDialog() == DialogResult.Cancel)
+            {
+                return;
+            }
+
+            string filename = saveFileDialog.FileName;
+
+            foreach (var item in processLV.Items)
+            {
+                System.IO.File.AppendAllText(filename,item.ToString());
+                System.IO.File.AppendAllText(filename, "\n");
+            }
+            MessageBox.Show("Файл сохранен");
+        }
+
+        private void exitClick(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
